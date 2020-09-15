fe03546300b027af50dc06326fd58ff33c45c8b1
dmitriyVahr <dmitriy.vahrushev@nutnet.ru>
2020-09-15 15:44:56 +0400

    build

---
+++ build/precache-manifest.7081fe3d1f7e56f25b22fbfe16370551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1303e44f408fb31880b4a8fdb592e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b757cbb19505a405b94",
+    "url": "/static/css/main.96f0a261.chunk.css"
+  },
+  {
+    "revision": "fb42e57a9d773e80d561",
+    "url": "/static/js/2.5bdb35cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bdb35cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b757cbb19505a405b94",
+    "url": "/static/js/main.d8aacdfd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/static/media/polygon.cc54c19b.jpg"
+  }
+]);
